04092153d7c043af5fec7e088e8c6ae18fd96e3f
vignesh1001 <vignesh1001@gmail.com>
2021-12-01 18:12:31 +0530

    1 file modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,22 @@
 /** @format */
 import { ThemeProvider } from '@mui/material/styles';
 import Head from 'next/head';
-import { Provider } from '../context';
 import theme from '../theme';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <Provider>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-          />
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          <title>Maaco</title>
-        </Head>
-        <Component {...pageProps} />
-      </Provider>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <title>Maaco</title>
+      </Head>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
